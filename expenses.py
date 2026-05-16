4ee0be8803143c896e3840b8a3421418f7ea033a
entite012-dev <entite012@gmail.com>
2026-05-16 20:45:18 +0530

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -1,8 +1,9 @@
 import csv
 import os
-
+#creating empty list
 a = []
 p = []
+#adding function and taking input 
 def add_expense():
     name = input("Name: ")
     food = int(input("Food expense: "))
@@ -10,6 +11,7 @@ def add_expense():
     a.append({"name": name, "food": food, "travel": travel})
     p.append(food+travel)
 
+#save function
 def save():
     file_exists = os.path.exists("expenses.csv")
     with open("expenses.csv", "a") as file:
@@ -18,15 +20,15 @@ def save():
             writer.writeheader()
         writer.writerows(a)
     print("Saved!")
-
+#view function 
 def view():
     print(a)
 
-
+#total function 
 
 def total():
     print(f"this is your sum of expenses {p}")
-
+# asking loop
 while True:
     print("1. Add  2. View  3. Exit 4. sum")
     x = input("Choice: ")
